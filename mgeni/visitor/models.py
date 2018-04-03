6cc2f527767bf8c3184eb3fcc6eed926d3cdb120
mburuBeatrice <mburubeatricewanjiru@gmail.com>
2018-04-03 14:57:52 +0300

    adds a method to create a visitor

--- mgeni/visitor/models.py
+++ mgeni/visitor/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.core.urlresolvers import reverse
 
 # Create your models here.
 class Visitor(models.Model):
@@ -6,13 +7,20 @@ class Visitor(models.Model):
 
     def __str__(self):
         return self.name
-    
+    def get_absolute_url(self):
+        return reverse("visitor:detail", kwargs={"id": self.id})
+    class Meta:
+        ordering = ["id"]
     def create_visitor(self):
         return self.save()
 
     def delete_visitor(self):
         return self.delete()
-    # @classmethod
-    # def update_visitor
-
-
+    @classmethod
+    def update_visitor(cls, id, name):
+        update_visitor = Visitor.objects.filter(id=id).update(name = name)
+        return update_visitor
+    @classmethod
+    def search(cls,search_term):
+        visitor_search = cls.objects.filter(name__name__icontains=search_term)
+        return visitor_search
