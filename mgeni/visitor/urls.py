a31b15978a761f4368e681b61ada2c3dce9150d5
mburuBeatrice <mburubeatricewanjiru@gmail.com>
2018-04-03 14:59:13 +0300

    adds paths for view functions

--- mgeni/visitor/urls.py
+++ mgeni/visitor/urls.py
@@ -1,7 +1,17 @@
 from django.conf.urls import url
 from . import views
 from django.conf import settings
+from django.conf.urls.static import static
+
 
 urlpatterns=[
     url(r'^$', views.index, name= 'index'),
+    url(r'^create/$', views.create, name='create'),
+    url(r'^(?P<id>\d+)/$',views.detail,name='detail'),
+    url(r'^(?P<id>\d+)/update$',views.update,name='update'),
+    url(r'^(?P<id>\d+)/delete$',views.delete,name='delete'),
 ]
+
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
